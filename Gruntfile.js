b22d47fb47e53d297d4fd6347fdc2961996503ae
David Calhoun <dcalhoun@centresource.com>
2014-02-17 09:12:47 -0600

    Copy icon files.

--- Gruntfile.js
+++ Gruntfile.js
@@ -267,8 +267,8 @@ module.exports = function (grunt) {
             '!**/_*{,/**}',
             // Explicitly add any files your site needs for distribution here
             //'_bower_components/jquery/jquery.js',
-            'favicon.ico'
-            // 'apple-touch*.png'
+            'favicon.ico',
+            'touch-icon*.png'
           ],
           dest: '<%= yeoman.dist %>'
         }]
